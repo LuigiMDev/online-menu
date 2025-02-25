668bd720503fd379cacc15626585fded72744e6f
LuigiMDev <luigicumaru@gmail.com>
2025-02-25 10:05:18 -0300

    feat: add form at finisheOrder

--- src/app/[slug]/menu/[productId]/components/FinishedOrderButton.tsx
+++ src/app/[slug]/menu/[productId]/components/FinishedOrderButton.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import {
   Drawer,
@@ -10,22 +11,108 @@ import {
   DrawerTrigger,
 } from "@/components/ui/drawer";
 import { Button } from "@/components/ui/button";
+import z from "zod";
+import { isValidCpf } from "../../helpers/cpf";
+import { useForm } from "react-hook-form";
+import { PatternFormat } from "react-number-format";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  name: z.string().trim().min(1, {
+    message: "O nome é obrigatório!",
+  }),
+  cpf: z
+    .string()
+    .trim()
+    .min(1, {
+      message: "O CPF é Obrigatório!",
+    })
+    .refine((value) => isValidCpf(value), {
+      message: "Digite um CPF válido!",
+    }),
+});
+
+type FormSchema = z.infer<typeof formSchema>;
 
 const FinishedOrderButton = () => {
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      cpf: "",
+    },
+  });
+
+  const onSubmit = (data: FormSchema) => {
+    console.log(data);
+  };
+
   return (
     <Drawer>
-      <DrawerTrigger className="w-full" asChild><Button className="w-full rounded-full">Finalizar pedido</Button></DrawerTrigger>
+      <DrawerTrigger className="w-full" asChild>
+        <Button className="w-full rounded-full">Finalizar pedido</Button>
+      </DrawerTrigger>
       <DrawerContent>
         <DrawerHeader>
           <DrawerTitle>Finalizar pedido</DrawerTitle>
-          <DrawerDescription>Insira suas informações abaixo para finalizar o seu pedido</DrawerDescription>
+          <DrawerDescription>
+            Insira suas informações abaixo para finalizar o seu pedido
+          </DrawerDescription>
         </DrawerHeader>
-        <DrawerFooter>
-          <Button>Submit</Button>
-          <DrawerClose>
-            <Button variant="outline">Cancel</Button>
-          </DrawerClose>
-        </DrawerFooter>
+        <div className="p-5">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Seu Nome</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Digite seu Nome..." {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="cpf"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Seu CPF</FormLabel>
+                    <FormControl>
+                      <PatternFormat
+                        placeholder="Digite o seu CPF..."
+                        format="###.###.###-##"
+                        customInput={Input}
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <DrawerFooter>
+                <Button type="submit">Submit</Button>
+                <DrawerClose>
+                  <Button variant="outline">Cancel</Button>
+                </DrawerClose>
+              </DrawerFooter>
+            </form>
+          </Form>
+        </div>
       </DrawerContent>
     </Drawer>
   );
